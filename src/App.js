c17108b54fea2b60db612e3ade2bf72b414ad370
DiliniNanayakkara <56678028+DiliniNanayakkara@users.noreply.github.com>
2022-05-04 14:58:26 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { React, useEffect, useState } from "react";
+import ScaleLoader from "react-spinners/ScaleLoader";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Pages/Home";
+import Gemspage from "./components/Pages/Gemspage";
+import Viewgem from "./components/Pages/Viewgem";
+import Birthstones from "./components/Pages/Birthstones";
+import Shippingcart from "./components/Pages/Shoppingcart";
+import Checkout from "./components/Pages/Checkout";
+import Shippingdetails from "./components/Pages/Shippingdetails";
+import Placeorder from "./components/Pages/Placeorder";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Reviews from "./components/reviews/reviews";
+import ErrorPage from "./components/Pages/Errorpage";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+
+      {loading ? (
+        <ScaleLoader
+          className="loader"
+          color={"#F5A623"}
+          loading={loading}
+          size={80}
+        />
+      ) : (
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/gemspage" element={<Gemspage />} />
+            <Route path="/viewgem" element={<Viewgem />} />
+            <Route path="/birthstones" element={<Birthstones />} />
+            <Route path="/shippingcart" element={<Shippingcart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/shippingdetails" element={<Shippingdetails />} />
+            <Route path="/placeorder" element={<Placeorder />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+          <Footer />
+        </Router>
+      )}
     </div>
   );
 }
